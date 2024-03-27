3c328272dddf0d96ecc115b885fbb54565086679
Pablo Gomez <pgomezna@gmail.com>
2024-03-27 16:41:11 +0100

    Hbase file loading

---
+++ delete_all_tables_hbase.py
@@ -0,0 +1,13 @@
+
+import happybase
+
+connection = happybase.Connection('192.168.1.47',port=9090) #Change IP to yours
+
+#s¡ha de fer a la vegada disable i delete, si a una taula li fas disable pero no delete es lia. 
+for table_name in connection.tables():
+    print(table_name)
+    connection.disable_table(table_name)
+    connection.delete_table(table_name)
+    print(f"Table '{table_name}' deleted")
+
+connection.close()
